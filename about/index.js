fecd54c712dd23bef563c55c2b13d9ddf54b9ef7
Clive Darra <cd@example.com>
2019-05-13 15:24:59 +0100

    ver 2

--- about/index.js
+++ about/index.js
@@ -9,7 +9,7 @@ app.use(helmet())
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.send(200, `
-        <h1><marquee direction=left>Hello from Express path '/about' on Now 2.0!</marquee></h1>
+        <h1>Hello from ZEIT NOW path '/about' on Now 2.0!</h1>
         <h2>Go to <a href="/">/</a></h2>
     `)
 })
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(helmet())
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
+        <h1>Hello from ZEIT NOW path '/' on Now 2.0!</h1>
         <h2>Go to <a href="/about">/about</a></h2>
     `)
 })
